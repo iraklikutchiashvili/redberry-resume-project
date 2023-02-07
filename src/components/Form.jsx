33fdce7864607f250ced865bbfa5d6498d1c27f0
iraklikutchiashvili <irakli9823@gmail.com>
2023-02-07 23:40:30 +0400

    Add validations to name,surname,email,phone. LEFT FILE UPLOAD VALIDATION!

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,16 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 
 function Form() {
+  const [validatNames, setValidatNames] = useState({
+    FName: false,
+    LName: false,
+    email: false,
+    phone: false,
+  });
+
+  const [values, setValues] = useState({
+    FName: "",
+    LName: "",
+    about: "",
+    email: "",
+    phone: "",
+  });
+
+  function isInputValid(event) {
+    const value = event.target.value;
+    const name = event.target.name;
+    let regex;
+    let lengthCondition;
+    if (name === "FName" || name === "LName") {
+      regex = /[ა-ჰ]+/;
+      lengthCondition = value.length < 2;
+    } else if (name === "email") {
+      regex = /^[a-zA-z]\w+@redberry.ge$/;
+    } else if (name === "phone") {
+      regex = /^\+9955\d+/;
+      lengthCondition = value.length === 9;
+    }
+    if (!regex.test(value) || lengthCondition) {
+      console.log(regex.test(value));
+      setValidatNames({
+        ...validatNames,
+        [name]: true,
+      });
+    } else {
+      setValidatNames({
+        ...validatNames,
+        [name]: false,
+      });
+    }
+  }
+
+  function handleChange(event) {
+    const value = event.target.value;
+    const name = event.target.name;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  }
+
   return (
     <div>
-      <Box>
+      <Box component="form" autoComplete="off">
         <div className="fullname">
           <div className="name">
             <TextField
+              value={values.FName}
+              name="FName"
+              onBlur={isInputValid}
+              onChange={handleChange}
+              error={validatNames.FName}
               id="standard-helperText"
               label="სახელი"
               placeholder="ანზორი"
@@ -22,6 +79,11 @@ function Form() {
 
           <div className="name">
             <TextField
+              value={values.LName}
+              name="LName"
+              onBlur={isInputValid}
+              onChange={handleChange}
+              error={validatNames.LName}
               id="standard-helperText"
               label="გვარი"
               placeholder="მგელაძე"
@@ -33,7 +95,7 @@ function Form() {
         </div>
         <Stack direction="row" alignItems="center" spacing={2}>
           <h4>პირადი ფოტოს ატვირთვა</h4>
-          <Button variant="contained" component="label">
+          <Button variant="contained" component="label" required>
             ატვირთვა
             <input hidden accept="image/*" multiple type="file" />
           </Button>
@@ -41,6 +103,9 @@ function Form() {
 
         <div className="abt-mail-phone">
           <TextField
+            name="about"
+            value={values.about}
+            onChange={handleChange}
             id="outlined-multiline-static"
             label="ჩემ შესახებ (არასავალდებულო)"
             multiline
@@ -49,6 +114,11 @@ function Form() {
             fullWidth={true}
           />
           <TextField
+            name="email"
+            onChange={handleChange}
+            onBlur={isInputValid}
+            error={validatNames.email}
+            value={values.email}
             id="standard-helperText"
             label="ელფოსტა"
             placeholder="anzor666@redberry.ge"
@@ -57,6 +127,11 @@ function Form() {
             fullWidth={true}
           />
           <TextField
+            name="phone"
+            onBlur={isInputValid}
+            onChange={handleChange}
+            error={validatNames.phone}
+            value={values.phone}
             id="standard-helperText"
             label="მობილურის ნომერი"
             placeholder="+995 555 55 55 55"
@@ -71,30 +146,6 @@ function Form() {
           </Button>
         </div>
       </Box>
-      {/* <form>
-        <div className="row">
-          <div className="col">
-            <label htmlFor="firstname">სახელი</label>
-            <input
-              id="firstname"
-              type="text"
-              className="form-control"
-              placeholder="სახელი"
-            />
-            <p>მინიმუმ 2 ასო ქართული ასოები</p>
-          </div>
-          <div className="col">
-            <label htmlFor="lastname">გვარი</label>
-            <input
-              id="lastname"
-              type="text"
-              className="form-control"
-              placeholder="გვარი"
-            />
-            <p>მინიმუმ 2 ასო ქართული ასოები</p>
-          </div>
-        </div>
-      </form> */}
     </div>
   );
 }
